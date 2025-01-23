f49dcc0e029e8e0edb9caa603098990e2825f667
Pipe Villa <jfvilla@gmail.com>
2025-01-23 11:00:05 -0500

    Adiciono comentarios explicando las funciones en los 2 micros

--- AWS/service_1/main.py
+++ AWS/service_1/main.py
@@ -6,24 +6,30 @@ import requests
 app = FastAPI()
 SECRET_KEY = "007secret"
 
+# Función para generar un token JWT
 @app.post("/generate-token/")
-def generate_token():
+def generate_token():    
+    # Definimos la carga útil del token con un mensaje y una expiración de 5 minutos    
     payload = {
         "Mission": "Valid token Agent 007, you accomplished the mission!",
         "exp": datetime.datetime.now() + datetime.timedelta(minutes=5)
     }
+    # Codificamos el token usando la clave secreta y el algoritmo HS256
     token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
     
     if token:
+        # Si el token se generó correctamente, nos comunicamos con el servicio 2
         result = communicate_with_service_2(token)
         return {
             "Response": result.get("Mission"),
             "Token": token  # Aquí incluimos el valor del token en la respuesta
         }
 
-
+# Función para comunicarse con el servicio 2 y validar el token
 def communicate_with_service_2(token):
     url = "http://service_2:8001/validate-token"
     headers = {"Authorization": f"Token: {token}"}
+    # Realizamos una solicitud POST al servicio 2 con el token en los encabezados
     response = requests.post(url, headers=headers)
+    # Retornamos la respuesta del servicio 2
     return response.json()
--- local/service_2/main.py
+++ local/service_2/main.py
@@ -4,16 +4,22 @@ import jwt
 app = FastAPI()
 SECRET_KEY = "007secret"
 
+# Endpoint para validar el token JWT
 @app.post("/validate-token/")
 def validate_token(Authorization: str = Header(None)):
+    # Verificamos si el encabezado de autorización está bien formado
     if not Authorization:
         raise HTTPException(status_code=401, detail="Token missing")
-
+    
+    # Extraemos el token del encabezado de autorización
     token = Authorization.split(" ")[1]  # <token>
     try:
+        # Decodificamos el token usando la clave secreta y el algoritmo HS256
         decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
         return {"Mission": "Valid token, you can pass!", "Mission": decoded["Mission"]}
     except jwt.ExpiredSignatureError:
+        # Manejo de excepción si el token ha expirado, >5min
         raise HTTPException(status_code=401, detail="Token expired, >5min... Try again agent!")
     except jwt.InvalidTokenError:
+        # Excepción si el token es inválido o malformado
         raise HTTPException(status_code=401, detail="Invalid token, you shall not pass!")
